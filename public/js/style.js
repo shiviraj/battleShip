4d62f90aa691edd256ee45f1684654a427b32c4d
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-02-25 22:11:05 +0530

    remove new game and added main view

--- public/js/style.js
+++ public/js/style.js
@@ -27,31 +27,21 @@ const prevPanel = function(showID, hideID, textToChange) {
   changeCardTitle(textToChange);
 };
 
-const playGame = function() {
-  const $playGame = document.querySelector('#play-game');
-  const $back = document.querySelector('#host-join #back');
-  $playGame.onclick = () => nextPanel('#main', '#host-join', 'Play Game');
-  $back.onclick = () => prevPanel('#main', '#host-join', 'Battle Ship');
-};
-
 const hostGame = function() {
   const $hostGame = document.querySelector('#host-game');
-  const $back = document.querySelector('#host-game-form .action #back');
-  $hostGame.onclick = () =>
-    nextPanel('#host-join', '#host-game-form', 'Host Game');
-  $back.onclick = () => prevPanel('#host-join', '#host-game-form', 'Play Game');
+  const $back = document.querySelector('#host-form #back');
+  $hostGame.onclick = () => nextPanel('#main', '#host-form', 'Host Game');
+  $back.onclick = () => prevPanel('#main', '#host-form', 'Battle Ship');
 };
 
 const joinGame = function() {
   const $joinGame = document.querySelector('#join-game');
-  const $back = document.querySelector('#join-game-form .action #back');
-  $joinGame.onclick = () =>
-    nextPanel('#host-join', '#join-game-form', 'Join Game');
-  $back.onclick = () => prevPanel('#host-join', '#join-game-form', 'Play Game');
+  const $back = document.querySelector('#join-form #back');
+  $joinGame.onclick = () => nextPanel('#main', '#join-form', 'Join Game');
+  $back.onclick = () => prevPanel('#main', '#join-form', 'Battle Ship');
 };
 
 const attachListener = function() {
-  playGame();
   // aboutGame();
   // settings();
   hostGame();
