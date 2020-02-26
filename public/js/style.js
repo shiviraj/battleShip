91c9b4b7d9fdcabc6792bd71cc201313286bfc89
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-02-26 08:36:19 +0530

    Added waiting page to join other players

--- public/js/style.js
+++ public/js/style.js
@@ -27,25 +27,62 @@ const prevPanel = function(showID, hideID, textToChange) {
   changeCardTitle(textToChange);
 };
 
-const hostGame = function() {
+const hostGameView = function() {
   const $hostGame = document.querySelector('#host-game');
   const $back = document.querySelector('#host-form #back');
   $hostGame.onclick = () => nextPanel('#main', '#host-form', 'Host Game');
   $back.onclick = () => prevPanel('#main', '#host-form', 'Battle Ship');
 };
 
-const joinGame = function() {
+const joinGameView = function() {
   const $joinGame = document.querySelector('#join-game');
   const $back = document.querySelector('#join-form #back');
   $joinGame.onclick = () => nextPanel('#main', '#join-form', 'Join Game');
   $back.onclick = () => prevPanel('#main', '#join-form', 'Battle Ship');
 };
 
+const showWaitingPage = function(data) {
+  const $loadingPage = getElement('#waiting');
+  const $gameID = getElement('.game-id #game-id');
+  const $joinedPlayer = getElement('.joined-player');
+  $loadingPage.classList.remove('hidden');
+  $gameID.innerHTML = data.gameID;
+  $joinedPlayer.innerHTML = `<span class="player">${data.username}</span>`;
+};
+
+const hideCard = function() {
+  const $card = getElement('.card');
+  $card.classList.add('hidden');
+};
+
+const hostGame = function() {
+  const $hostForm = getElement('#host-form');
+  $hostForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const username = getElement('#host-form #username').value;
+    if (!username) return;
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({username})
+    };
+    fetch('hostGame', options)
+      .then(response => response.json())
+      .then(body => {
+        if (!body) return;
+        showWaitingPage(body);
+        hideCard();
+      });
+  });
+};
+
 const attachListener = function() {
   // aboutGame();
   // settings();
+  hostGameView();
+  joinGameView();
   hostGame();
-  joinGame();
+  // joinGame();
 };
 
 const main = function() {
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const publicDirectoryPath = path.join(__dirname, 'public');
-
 const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.static(publicDirectoryPath));
+app.use(express.json({limit: '100kb'}));
+
+app.post('/hostGame', (req, res) => {
+  const username = req.body.username;
+  res.setHeader('Content-Type', 'application/json');
+  res.send({username, gameID: Math.floor(Math.random() * 100000)});
+});
 
 app.listen(port, () => console.log(`server is listening on port ${port}`));
